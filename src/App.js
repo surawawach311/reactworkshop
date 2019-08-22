0e4c38dfbaa34fff97ae8a71c01159eca768635e
Surawach Saphapukdee <Surawach311@gmail.com>
2019-08-22 11:00:24 +0700

    add pa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-5">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn btn-primary btn-lg">Boom!</button>
+        
+        
       </header>
     </div>
   );
